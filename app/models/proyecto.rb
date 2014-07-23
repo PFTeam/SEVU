bcb077cc693bae2ebcecf62a6e746f107de2f795
Joel Noguera <Noguera.sistemas@gmail.com>
2014-07-23 00:56:56 -0300

    Estados Activo mejorado v2

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -25,7 +25,9 @@ class Proyecto < ActiveRecord::Base
 
 
   def self.activos
-    Proyecto.joins(:historial_estado_proyectos).where('historial_estado_proyectos.esActual' => 'True').includes(:estado_proyectos).where('estado_proyectos.nombre' => 'Activo')
+  #  Proyecto.joins(:historial_estado_proyectos).where('historial_estado_proyectos.esActual' => 'True').includes(:estado_proyectos).where('estado_proyectos.nombre' => 'Activo')
+    
+    Proyecto.includes(:historial_estado_proyectos, :estado_proyectos).where('historial_estado_proyectos.esActual' => 'True','estado_proyectos.nombre' => 'Activo')
     
   end
 
