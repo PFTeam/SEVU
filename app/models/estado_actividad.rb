7c6706346f5c38928bb35806da9c0674c5ee2476
Joel Noguera <Noguera.sistemas@gmail.com>
2014-08-03 16:25:54 -0300

    estados_posibles de las actividades

--- app/models/estado_actividad.rb
+++ app/models/estado_actividad.rb
@@ -5,4 +5,20 @@ class EstadoActividad < ActiveRecord::Base
 
   validates :nombre, :uniqueness => { :message => 'No pueden existir dos Estados con el mismo nombre', :case_sensitive => false  }, :length => { :maximum => 250 }, :presence => true
   validates :descripcion, :presence => true, :length => { :maximum => 250 }
+
+
+
+  def self.estados_posibles estado_actual
+    if estado_actual.nombre == 'Creada'
+      estadosPosibles = [EstadoProyecto.find_by(nombre: 'Ejecutada'), EstadoProyecto.find_by(nombre: 'Planificada'), estado_actual]
+    elsif estado_actual.nombre == 'Ejecutada'
+      estadosPosibles = [EstadoProyecto.find_by(nombre: 'Terminado'), estado_actual]
+    elsif estado_actual.nombre == 'Planificada'
+      estadosPosibles = [EstadoProyecto.find_by(nombre: 'Ejecutada'), estado_actual]
+    elsif estado_actual.nombre == 'Terminada'
+      estadosPosibles = [estado_actual]
+    else
+      estadosPosibles = [estado_actual]
+    end
+  end
 end
