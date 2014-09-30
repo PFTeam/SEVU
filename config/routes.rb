5579e416844d53d79919f8244539f4e9a8dfbe13
Joel Noguera <noguera.sistemas@gmail.com>
2014-09-30 18:31:19 +0200

    modificaciones al menu vertical y al autocompletado

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,11 @@ Rails.application.routes.draw do
     resources :detalle_restricciones #, only: [:new, :create]
   end
 
-  resources :postulaciones
+  resources :postulaciones do
+	  member do
+	    post 'aceptar'
+	  end
+  end
 
   resources :notas
 
@@ -69,14 +73,20 @@ Rails.application.routes.draw do
 
   resources :actividades
 
-  resources :objetivo_especificos
+  resources :objetivo_especificos do
+    resources :actividades
+  end
 
-  resources :objetivo_generales
+  resources :objetivo_generales do
+    resources :objetivo_especificos
+  end
 
   resources :historial_estado_proyectos
 
   resources :proyectos do
     resources :asignacion_roles
+    resources :objetivo_generales
+    resources :postulaciones
   end
 
   resources :necesidades
