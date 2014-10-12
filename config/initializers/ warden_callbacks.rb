4c5a0310caceb5ea606b99cc03e75caebf9212ac
Diego <sebadiegomza@gmail.com>
2014-10-12 13:51:40 +0200

    auditoria por usuario y por proyecto terminado

--- config/initializers/ warden_callbacks.rb
+++ config/initializers/ warden_callbacks.rb
@@ -1,12 +1,20 @@
 Warden::Manager.after_set_user(except: :fetch) do |record, warden, options|
-  Sesion.create!(
+  sesion = Sesion.create!(
     usuario_id: record.id,
     fechaInicio: Time.new, 
 		fechaFin: nil
+  )
+	Transaccion.create!(
+    descripcion: 'Inicio de sesión',
+    sesion_id: sesion.id
   )
 end
 
 Warden::Manager.before_logout do |record, warden, options|
 	sesion= Sesion.find_by(usuario_id: record.id, fechaFin: nil)
 	Sesion.update(sesion.id, :fechaFin => Time.new)
+	Transaccion.create!(
+    descripcion: 'Fin de sesión',
+    sesion_id: sesion.id
+  )
 end
